d595a017388df438a27c6e5e228f43761c15759a
Ana <110035519+AnnaShambe@users.noreply.github.com>
2023-02-23 19:44:21 +0400

    homework

---
+++ script.js
@@ -0,0 +1,33 @@
+// davaleba N1
+
+let item = 100;
+
+if (item < 50){
+    console.log('naklebia 50_ze');
+}else if (item > 20){
+    console.log('metia 20_ze');
+}else{
+    console.log('shecdoma');
+}
+
+// davaleba N2
+
+let UserName = 'mariam'
+let result = (UserName == 'mariam') ? 'true' : 'false';
+
+console.log(result);
+
+// davaleba N3
+
+let userName = 'mariami';
+
+switch (userName){
+    case "mariami":
+    console.log("true");
+    break;
+    case "ana":
+    console.log('name is ana');
+    break;
+    default:
+    console.log('false');
+}
